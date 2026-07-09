5079b8cb708ef3f6810264a40345247eaf569a94
Aditya Tripathi <aditya@climactic.co>
2026-07-09 05:59:02 +0000

    feat(facebook): comment fetch/reply/like/delete engagement connector

--- app/Services/Engagement/EngagementConnectorRegistry.php
+++ app/Services/Engagement/EngagementConnectorRegistry.php
@@ -6,6 +6,7 @@ namespace App\Services\Engagement;
 
 use App\Enums\Platform;
 use App\Services\Engagement\Connectors\BlueskyEngagementConnector;
+use App\Services\Engagement\Connectors\FacebookEngagementConnector;
 use App\Services\Engagement\Connectors\LinkedInEngagementConnector;
 use App\Services\Engagement\Connectors\XEngagementConnector;
 use App\Services\Engagement\Contracts\EngagementConnector;
@@ -18,7 +19,8 @@ class EngagementConnectorRegistry
             Platform::X => app(XEngagementConnector::class),
             Platform::Bluesky => app(BlueskyEngagementConnector::class),
             Platform::LinkedIn => app(LinkedInEngagementConnector::class),
-            Platform::Facebook, Platform::Instagram, Platform::Threads => throw new \LogicException(
+            Platform::Facebook => app(FacebookEngagementConnector::class),
+            Platform::Instagram, Platform::Threads => throw new \LogicException(
                 "Engagement connector for {$platform->value} is not implemented yet.",
             ),
         };
